7d9f64a7bfbd8a4dbc8bb00ec9e105b621fe764e
amin10003 <maxamedxasancali10003@gmail.com>
2025-08-12 21:46:53 +0300

    added fragment and h2

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -1,6 +1,12 @@
+import { Fragment } from "react";
+
 function Greeting(props) {
-    return <h1>Hello, {props.name} {props.surname}  !</h1>;
+    return (
+        <>
+            <h1>Hello, {props.name} {props.surname}!</h1>
+            <h2>This text was not there originally</h2>
+        </>
+    );
 }
 
-
-export default Greeting
+export default Greeting;
