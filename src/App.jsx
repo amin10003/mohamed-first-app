9f4aadfcb3c1811d843291905bdea768a5da3c40
amin10003 <maxamedxasancali10003@gmail.com>
2025-08-12 20:34:14 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/Header'
+import Greeting from './components/Greetings'
+function App() {
+  return (
+    <>
+    <Greeting name = "Mohamed" surname = "Hassan" />
+
+   <Header/>
+    <Header/>
+     <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+         <h1>this is my first react app</h1>
+    </div>
+  )
+}
+
+export default Header
